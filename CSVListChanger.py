e8f1dcf3d6f5ac2e82731b026bbdb7b28c67d3a6
kkkkoyo <mmkrz.sg@gmail.com>
2017-05-25 18:53:05 +0900

    initialcommit

---
+++ CSVListChanger.py
@@ -0,0 +1,31 @@
+#coding:utf-8
+from __future__ import print_function
+from __future__ import unicode_literals
+import argparse
+import csv
+import io
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("csv_file_path")
+parser.add_argument("--encoding", default="utf_8")
+options = parser.parse_args()
+
+csv_reader = csv.reader(
+     io.open(options.csv_file_path, "r", encoding=options.encoding),
+)
+
+for i,row in enumerate(csv_reader):
+    row.pop() #一つ余計だからpopする
+    sys.stdout.write("[")
+    for j,data in enumerate(row):
+        if (j==0):
+            sys.stdout.write('\" '+data+'\",')
+        elif (j==len(row)-1):
+            sys.stdout.write('\"'+data+'\"')
+        else:
+            sys.stdout.write('\"'+data+'\",')
+    if(i==20): #最後は点を取る
+        print("] ")
+    else:
+        print("], ")
